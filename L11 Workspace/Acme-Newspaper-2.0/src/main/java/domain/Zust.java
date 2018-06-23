10c8ef46afb4194deb6c3dc2ea3e797602513d44
HisperianHub <Mario.biz.gon@gmail.com>
2018-06-23 20:13:01 +0200

    Arreglado Populate.xml

--- L11 Workspace/Acme-Newspaper-2.0/src/main/java/domain/Zust.java
+++ L11 Workspace/Acme-Newspaper-2.0/src/main/java/domain/Zust.java
@@ -87,11 +87,11 @@ public class Zust extends DomainEntity {
 		this.moment = moment;
 	}
 
-	public boolean isFinal() {
+	public boolean getIsFinal() {
 		return this.isFinal;
 	}
 
-	public void setFinal(final boolean isFinal) {
+	public void setIsFinal(final boolean isFinal) {
 		this.isFinal = isFinal;
 	}
 
